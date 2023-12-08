19e15812455312f932ef78bdef66454cefb24504
dale.partridge@gmail.com <dapa@pml.ac.uk>
2023-12-08 11:49:35 +0000

    Fix errors in submit_job.sh

--- cycle/submit_job.sh
+++ cycle/submit_job.sh
@@ -8,27 +8,35 @@ nday=`cal $month $year | grep -v '[A-Za-z]' | wc -w`
 iter_end=$(($iter_start + 86400*$nday/$dt))
 iter_start=$(($iter_start + 1))
 
-$CYCLE_DIR/update_nemo_nl --file $RUN_DIR/namelist_cfg  \
+mm=$(printf '%02d' $month)
+
+$CYCLE_DIR/update_nemo_nl --phy_file $RUN_DIR/namelist_cfg  \
     --runid $NAME                 \
     --restart true            \
     --next_step $iter_start           \
     --final_step $iter_end          \
-    --restart_file ${NAME}_${year}${month}01_restart \
+    --restart_file ${NAME}_${year}${mm}01_restart \
     --trc_file $RUN_DIR/namelist_top_cfg  \
-    --trc_restart_file ${NAME}_${year}${month}01_restart_trc
-if [$ICE]
+    --trc_restart_file ${NAME}_${year}${mm}01_restart_trc
+if [ $ICE = true ] ; then
     $CYCLE_DIR/update_nemo_nl \
     --ice_file $RUN_DIR/namelist_ice_cfg  \
-    --ice_restart_file ${NAME}_${year}${month}01_restart_ice
+    --ice_restart_file ${NAME}_${year}${mm}01_restart_ice
 fi
+if [ $iter_start -eq 1 ]; then
+    $CYCLE_DIR/update_nemo_nl --phy_file $RUN_DIR/namelist_cfg  \
+    --restart false
+fi
+
 # Launch Run
-echo "Launching $year $month at $(date +%s) seconds since 1970-01-01 00:00:00"
+echo "Launching $year $month at $(date +'%F %T')"
+#srun runscript.slurm
 $RUN_DIR/runscript.slurm
 
 # Archive results
-outdir=$OUTPUT_DIR/$year/$month
+outdir=$OUTPUT_DIR/$year/$mm
 mkdir -p $outdir
 mv $NAME*nc *.output $outdir
-cp namelist* fabm.yaml fabm_input.nml $outdir
+cp namelist* $outdir
 
 echo $iter_end > $RUN_DIR/current_iter
