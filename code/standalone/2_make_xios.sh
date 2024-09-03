81aabf1afd618a50b5289ff537c7db688ff7342a
Yuri Artioli <yuri@pml.ac.uk>
2024-09-03 16:50:35 +0100

    added extraction of XIOS tools before copying Config.pm

--- code/standalone/2_make_xios.sh
+++ code/standalone/2_make_xios.sh
@@ -5,13 +5,22 @@
 # Note that a precompiled version of XIOS2.5 is available for use here:
 # /work/n01/shared/acc/xios-2.5
 #################################################
-
+cd $XIOS_CLONE
 if [ "$ARCHER2" = true ] ; then
 yes | cp $CODE_DIR/archer2-files/xios/arch-X86_ARCHER2-Cray* $XIOS_CLONE/arch/
+for tarname in `ls $XIOS_CLONE/tools/archive/*.tar.gz`
+do
+   if  ( [[ ${tarname} == "$XIOS_CLONE/tools/archive/boost.tar.gz" ]] && [[ "$use_extern_boost" == "true" ]] ) || ( [[ ${tarname} == "$XIOS_CLONE/tools/archive/blitz.tar.gz" ]] && [[ "$use_extern_blitz" == "true" ]] )
+   then
+	   continue
+   fi
+   gunzip -f "$tarname"
+   tar -xf ${tarname%.gz}
+done
 yes | cp $CODE_DIR/archer2-files/xios/Config_cray.pm $XIOS_CLONE/tools/FCM/lib/Fcm/Config.pm
 fi
 
-cd $XIOS_CLONE && ./make_xios --prod --arch $XIOS_ARCH --netcdf_lib netcdf4_par --job 16 --full
+./make_xios --prod --arch $XIOS_ARCH --netcdf_lib netcdf4_par --job 16 --full
 rsync -a $XIOS_CLONE/bin $XIOS_CLONE/inc $XIOS_CLONE/lib $XIOS_HOME
 
 cp $XIOS_HOME/bin/xios_server.exe $EXECUTABLE_DIR/xios
