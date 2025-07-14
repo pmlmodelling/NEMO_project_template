f7bfbc77c305d7374c2289c0fa3174dc8cf89ffb
Rebecca Millington <rmi@scyllalg01.head.cm.hpcm.npm.ac.uk>
2025-07-14 17:15:17 +0100

    changing paths for ersem and nemo repositories

--- scripts/core-scripts/1_clone_ersem_fabm.sh
+++ scripts/core-scripts/1_clone_ersem_fabm.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/../config.sh
 
 # Clone code bases for ERSEM, FABM
-ERSEM_REV=8510fbf80a4ef8b1f9f827e156531e9e0ce6315b
-git clone https://github.com/pmlmodelling/ersem.git $ERSEM_DIR
+ERSEM_REV=combined
+git clone git@github.com:pmlmodelling/ersem-neccton.git $ERSEM_DIR
 cd $ERSEM_DIR && git checkout $ERSEM_REV
 
 FABM_REV=2178e4198586578664ca8db21b508c52cf5d3b83
