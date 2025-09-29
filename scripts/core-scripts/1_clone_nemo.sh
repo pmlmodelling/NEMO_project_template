08407bf590d0669f7ce06eb79c5245134f276edb
Helen Powley <78801223+hpowley@users.noreply.github.com>
2025-09-29 10:50:22 +0100

    Revert  nemo compilation back to master

--- scripts/core-scripts/1_clone_nemo.sh
+++ scripts/core-scripts/1_clone_nemo.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/../config.sh
 
-NEMO_REV=sinking_rate
-git clone -b $NEMO_REV https://github.com/pmlmodelling/NEMO4.0-FABM.git $NEMO_DIR
-
+NEMO_REV=a83ab4cec06bf4302c6873d4f094228955819ca4
+git clone https://github.com/pmlmodelling/NEMO4.0-FABM.git $NEMO_DIR
+cd $NEMO_DIR && git checkout $NEMO_REV
 
