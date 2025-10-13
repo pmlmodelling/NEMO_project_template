ccfa2b55b1fa76b2dd91315ce4b6b4fa28e9497b
Rebecca Millington <rmi@scyllalg01.head.cm.hpcm.npm.ac.uk>
2025-10-13 14:33:28 +0100

    adding files to run with new version of neccton

--- scripts/core-scripts/1_clone_nemo.sh
+++ scripts/core-scripts/1_clone_nemo.sh
@@ -3,8 +3,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/../config.sh
 
-NEMO_REV=a83ab4cec06bf4302c6873d4f094228955819ca4
-git clone https://github.com/pmlmodelling/NEMO4.0-FABM.git $NEMO_DIR
-cd $NEMO_DIR && git checkout $NEMO_REV
+#NEMO_REV= "sinking_rate"  #a83ab4cec06bf4302c6873d4f094228955819ca4
+git clone -b sinking_rate https://github.com/pmlmodelling/NEMO4.0-FABM.git $NEMO_DIR
+#cd $NEMO_DIR && git checkout $NEMO_REV
 
 
