8a2543ff41230180ed5f4263e3e49f73aa3209b7
Rebecca Millington <rmi@scyllalg01.head.cm.hpcm.npm.ac.uk>
2025-11-14 16:52:06 +0000

    updating nemo version

--- scripts/core-scripts/1_clone_nemo.sh
+++ scripts/core-scripts/1_clone_nemo.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/../config.sh
 
-NEMO_REV=sinking_rate
-git clone -b $NEMO_REV https://github.com/pmlmodelling/NEMO4.0-FABM.git $NEMO_DIR
-
+NEMO_REV= a6991353da8fcf4ac0a043ba0d0eddd87bc2386f
+git clone https://github.com/pmlmodelling/NEMO4.0-FABM.git $NEMO_DIR
+cd $NEMO_DIR && git checkout $NEMO_REV
 
