45fcffdd6644aa1f13847b7374008d43c99c7b74
Michael Wathen <miwa@scyllalg01.head.cm.hpcm.npm.ac.uk>
2026-03-18 15:05:20 +0000

    Moving ebm_a0, ebm_a1, ebm_WM_flag to parameter netcdf

--- code/EBM_code/sbcrnf_ebm.F90
+++ code/EBM_code/sbcrnf_ebm.F90
@@ -30,13 +30,16 @@ MODULE sbcrnf_ebm
    PUBLIC   sbc_rnfebm       ! called in sbcmod module
    PUBLIC   sbc_rnfebm_init  ! called in sbcmod module
 
-   INTEGER , PARAMETER ::  jpfld = 6            ! Number of EBM parameters
+   INTEGER , PARAMETER ::  jpfld = 9            ! Number of EBM parameters
    INTEGER , PARAMETER ::  jp_msk = 1           ! index of msk parameter
    INTEGER , PARAMETER ::  jp_L = 2           ! index of L parameter
    INTEGER , PARAMETER ::  jp_W = 3           ! index of W parameter
    INTEGER , PARAMETER ::  jp_H = 4           ! index of H parameter
    INTEGER , PARAMETER ::  jp_uH = 5           ! index of uH parameter
    INTEGER, PARAMETER :: jp_angle = 6
+   INTEGER, PARAMETER :: jp_a0 = 7
+   INTEGER, PARAMETER :: jp_a1 = 8
+   INTEGER, PARAMETER :: jp_WM_flag = 9
    TYPE(FLD), ALLOCATABLE, DIMENSION(:) ::   sf_ebm       ! structure: EBM data
    TYPE(FLD_N), DIMENSION(jpfld) ::   sn_ebm         ! array of namelist information on files to be read
 
@@ -47,9 +50,12 @@ MODULE sbcrnf_ebm
    TYPE(FLD_N)                ::   sn_ebm_H          !: information about the EBM estuary heights to be read
    TYPE(FLD_N)                ::   sn_ebm_uH          !: information about the EBM estuary upper layer heights to be read
    TYPE(FLD_N) :: sn_ebm_angle
+   TYPE(FLD_N) :: sn_ebm_a0
+   TYPE(FLD_N) :: sn_ebm_a1
+   TYPE(FLD_N) :: sn_ebm_WM_flag
 
    REAL(wp), ALLOCATABLE, DIMENSION(:,:) :: ebm_H_ocean, ebm_H_chan, ebm_L_chan, ebm_W_mouth, ebm_V_est
-   REAL(wp), ALLOCATABLE, DIMENSION(:,:) :: ebm_u_tide, ebm_L_tide, ebm_Q_river, ebm_S_ocean, ebm_T_ocean, ebm_a0
+   REAL(wp), ALLOCATABLE, DIMENSION(:,:) :: ebm_u_tide, ebm_L_tide, ebm_Q_river, ebm_S_ocean, ebm_T_ocean, ebm_a0, ebm_a1
    REAL(wp), ALLOCATABLE, DIMENSION(:,:) :: ebm_angle
    INTEGER , ALLOCATABLE, DIMENSION(:,:) :: ebm_wide_mouth, ebm_river_mask
 
@@ -68,7 +74,7 @@ CONTAINS
       ALLOCATE( &
            & ebm_H_ocean(jpi,jpj), ebm_H_chan(jpi,jpj), ebm_L_chan(jpi,jpj), ebm_W_mouth(jpi,jpj), ebm_V_est(jpi,jpj), &
            & ebm_u_tide(jpi,jpj), ebm_L_tide(jpi,jpj), ebm_Q_river(jpi,jpj), ebm_S_ocean(jpi,jpj), &
-           & ebm_T_ocean(jpi,jpj), ebm_a0(jpi,jpj), ebm_angle(jpi,jpj), &
+           & ebm_T_ocean(jpi,jpj), ebm_a0(jpi,jpj), ebm_a1(jpi,jpj), ebm_angle(jpi,jpj), &
            & ebm_wide_mouth(jpi,jpj), ebm_river_mask(jpi,jpj), &
            & ebm_Q_UM(jpi,jpj), ebm_Q_LM(jpi,jpj), ebm_S_UM(jpi,jpj), ebm_const(jpi,jpj), ebm_rho_UM(jpi,jpj), ebm_S_diff(jpi,jpj), &
            & STAT=sbc_rnfebm_alloc )
@@ -110,6 +116,8 @@ CONTAINS
       ebm_H_ocean(:,:) = sf_ebm(jp_H )%fnow(:,:,1)
       ebm_H_chan(:,:)  = sf_ebm(jp_uH)%fnow(:,:,1)
       ebm_angle(:,:)   = sf_ebm(jp_angle)%fnow(:,:,1)
+      ebm_a0(:,:)      = sf_ebm(jp_a0)%fnow(:,:,1)
+      ebm_a1(:,:)      = sf_ebm(jp_a1)%fnow(:,:,1)
       ebm_S_diff(:,:) = 0._wp
 
       ebm_V_est(:,:)   = ebm_L_chan(:,:) * ebm_W_mouth(:,:) * ebm_H_ocean(:,:)
@@ -229,27 +237,23 @@ CONTAINS
       ! Convert runoff to discharge (m3/s). Guard against negative/outflow values.
       ebm_Q_river(:,:) = MAX( 0._wp, rnf(:,:) * e1t(:,:) * e2t(:,:) / 1000.0 )
 
-      ! Empirical a0 field: set to 0 to trigger model-default a_0 inside EBM
-      ebm_a0(:,:) = 0._wp
-
-      ! Wide-mouth flag currently disabled
-      ebm_wide_mouth(:,:) = 1
+      ebm_wide_mouth(:,:) = NINT( sf_ebm(jp_WM_flag)%fnow(:,:,1) )
 
       ! Authoritative mask: only compute where the external mask is active and runoff is positive
       !ebm_river_mask(:,:) = ( sf_ebm(jp_msk)%fnow(:,:,1) == 1._wp ) .AND. ( ebm_Q_river(:,:) > 0._wp )
       ebm_river_mask(:,:) = sf_ebm(jp_msk)%fnow(:,:,1)
-        
+
 
       CALL ebm%load_estuary( ebm_H_ocean, ebm_H_chan, ebm_L_chan, ebm_W_mouth, ebm_V_est, &
-         &                 ebm_u_tide, ebm_L_tide, ebm_Q_river, ebm_S_ocean, ebm_T_ocean, ebm_a0, &
+         &                 ebm_u_tide, ebm_L_tide, ebm_Q_river, ebm_S_ocean, ebm_T_ocean, ebm_a0, ebm_a1, &
          &                 ebm_wide_mouth, ebm_river_mask )
 
       CALL ebm%evaluate_box_model( Q_UM_out=ebm_Q_UM, Q_LM_out=ebm_Q_LM, S_UM_out=ebm_S_UM, &
          &                        const_out=ebm_const, rho_UM_out=ebm_rho_UM )
-        
+
 
       ! Convert EBM outflow into discharge load
-      ebm_Q_UM = 1000.0 * ebm_Q_UM(:,:) / (e1t(:,:) * e2t(:,:)) 
+      ebm_Q_UM = 1000.0 * ebm_Q_UM(:,:) / (e1t(:,:) * e2t(:,:))
 
 
       DO jj = 1, jpj
@@ -277,7 +281,7 @@ CONTAINS
 
       ! Update runoff and salinity
       rnf(:,:) = ebm_Q_UM(:,:)
-      rnf_tsc(:,:,jp_sal) = ebm_S_UM(:,:) * rnf(:,:) / 1000.0 
+      rnf_tsc(:,:,jp_sal) = ebm_S_UM(:,:) * rnf(:,:) / 1000.0
 
    END SUBROUTINE sbc_rnfebm
 
@@ -297,7 +301,8 @@ CONTAINS
 
       !!
       NAMELIST/namsbc_rnfebm/ cn_dir, &
-            & sn_ebm_msk, sn_ebm_L, sn_ebm_W, sn_ebm_H, sn_ebm_uH, sn_ebm_angle
+            & sn_ebm_msk, sn_ebm_L, sn_ebm_W, sn_ebm_H, sn_ebm_uH, sn_ebm_angle, &
+            & sn_ebm_a0, sn_ebm_a1, sn_ebm_WM_flag
 
       !!----------------------------------------------------------------------
       !
@@ -332,6 +337,9 @@ CONTAINS
       sn_ebm(jp_H) = sn_ebm_H
       sn_ebm(jp_uH) = sn_ebm_uH
       sn_ebm(jp_angle) = sn_ebm_angle
+      sn_ebm(jp_a0) = sn_ebm_a0
+      sn_ebm(jp_a1) = sn_ebm_a1
+      sn_ebm(jp_WM_flag) = sn_ebm_WM_flag
 
       ! Create structure for EBM parameter data
       ALLOCATE( sf_ebm(jpfld), STAT=ierror )         ! Create sf_rnf structure (runoff inflow)
@@ -347,7 +355,7 @@ CONTAINS
       ENDDO
 
       CALL fld_read ( nit000, nn_fsbc, sf_ebm )
-      
+
       IF( .NOT. ll_ebm_ready ) THEN
          CALL ebm%init()
          ll_ebm_ready = .TRUE.
