e7753c8f40ed993cd6a28a71d1ebb88032213050
Dale Partridge <dapa@scyllalg01.head.cm.hpcm.npm.ac.uk>
2026-07-16 15:45:13 +0100

    Pull latest NEMO version

--- scripts/compile-scripts/1_clone_nemo.sh
+++ scripts/compile-scripts/1_clone_nemo.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/../config.sh
 
-NEMO_REV=91f1b08dd8fa6dd62ea302fb2463689f6f6de92e
+NEMO_REV=8819de21b78f59d44bfbfa95c0be97c72b88b3e7
 git clone https://github.com/pmlmodelling/NEMO4.0-FABM.git $NEMO_DIR
 cd $NEMO_DIR && git checkout $NEMO_REV
 
